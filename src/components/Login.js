3113d3d7381e1d0cf9fd210ca3aed750003791f0
iemtabish <tabishabbasmzp@gmail.com>
2025-09-07 00:35:16 +0530

    Sign In/Up form, Header, Login page, Logo

---
+++ src/components/Login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+const Login = () => {
+  const [isSigninForm, setisSigninform] = useState(true); {/*Ni smj aya ye code*/}
+
+const toggleSignInForm = () => { {/*Ni smj aya ye code*/}
+    setisSigninform(!isSigninForm); {/*Ni smj aya ye code*/}
+}
+
+  return (
+    <div >
+      <Header/>
+    <div className="absolute">
+      <img src='https://assets.nflxext.com/ffe/siteui/vlv3/8d617e19-3c3c-4c28-8998-c9b14dbc7200/web/IN-en-20250901-TRIFECTA-perspective_48d84d4e-9558-46b8-a0f3-8b2dc8478431_medium.jpg'
+       alt='bg'/>
+    </div>
+
+ <div className="absolute inset-20 flex items-center justify-center">
+       <form className="flex flex-col w-3/12 p-12 bg-black bg-opacity-60 rounded text-white">
+       <h1 className='font-semibold py-6 text-white text-3xl'>{isSigninForm? "Sign In" : "Sign Up"}</h1>
+        {!isSigninForm && <input type="text" placeholder="Full Name" className="p-4 mb-8 w-full bg-black bg-opacity-70 text-white border border-white border-opacity-40 rounded"/>}
+        <input type="text"  placeholder='Email or phone number' className="p-4 mb-8 w-full bg-black bg-opacity-70 text-white border border-white border-opacity-40 rounded"/>
+        <input type="password"  placeholder='Password' className="p-4 mb-8 w-full bg-black bg-opacity-70 text-white border border-white border-opacity-40 rounded"/>
+        <button className= "p-4 mt-3 bg-red-600 w-full rounded-lg" >"{isSigninForm? "Sign In" : "Sign Up"}"</button> {/*Ni smj aya ye code*/}
+        <p className='py-6 m-1 cursor-pointer' onClick={toggleSignInForm}>{isSigninForm? "New to Netflix ? Sign Up Now" : "Already Registered ? Sign In Now"}</p>
+       </form>
+
+
+</div>
+    </div>
+
+  )
+}
+
+export default Login; 
